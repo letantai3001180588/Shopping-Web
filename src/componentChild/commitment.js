e623fd3e02105638d8ef22754f2b44613806b73c
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-06-14 21:27:28 +0700

    update

--- src/componentChild/commitment.js
+++ src/componentChild/commitment.js
@@ -8,8 +8,8 @@ export default function Commitment(){
                             <div className="card h-100 cart-product"  style={{display:'flex',justifyContent:'center',alignContent:'center',textAlign:'center',paddingTop:20,paddingBottom:20,background:'white',borderRadius:10+'px'}}>
                                 <div className="row">
                                     <i className="bi bi-shield" style={{fontSize:70}}></i>
-                                    <h5>Bao mat thanh toan</h5>
-                                    <p>Website cam ket bao mat thong tin khach hang</p>
+                                    <h5>Bảo mật thanh toán</h5>
+                                    <p>Website cam kết bảo mật thông tin khách hàng</p>
                                 </div>
                             </div>
                         </div>
@@ -18,8 +18,8 @@ export default function Commitment(){
                             <div className="card h-100 cart-product"  style={{display:'flex',justifyContent:'center',alignContent:'center',textAlign:'center',paddingTop:20,paddingBottom:20,background:'white',borderRadius:10+'px'}}>
                                 <div className="row">
                                     <i className="bi bi-arrow-counterclockwise" style={{fontSize:70}}></i>
-                                    <h5>Hoan tra</h5>
-                                    <p>Cho phep khach hang doi tra trong vong 90 ngay</p>
+                                    <h5>Hoàn trả</h5>
+                                    <p>Cho phép khách hàng đổi trả trong vòng 90 ngày</p>
                                 </div>
                             </div>
                         </div>
@@ -28,8 +28,8 @@ export default function Commitment(){
                             <div className="card h-100 cart-product"  style={{display:'flex',justifyContent:'center',alignContent:'center',textAlign:'center',paddingTop:20,paddingBottom:20,background:'white',borderRadius:10+'px'}}>
                                 <div className="row">
                                     <i className="bi bi-truck" style={{fontSize:70}}></i>
-                                    <h5>Van chuyen toan quoc</h5>
-                                    <p>Free ship toan quoc, co ho tro SHIP CODE cho khach hang</p>
+                                    <h5>Vận chuyển toàn quốc</h5>
+                                    <p>Free ship toàn quốc, có hỗ trợ SHIP CODE cho khách hàng</p>
                                 </div>
                             </div>
                         </div>
@@ -38,8 +38,8 @@ export default function Commitment(){
                             <div className="card h-100 cart-product"  style={{display:'flex',justifyContent:'center',alignContent:'center',textAlign:'center',paddingTop:20,paddingBottom:20,background:'white',borderRadius:10+'px'}}>
                                 <div className="row">
                                     <i className="bi bi-wechat" style={{fontSize:70}}></i>
-                                    <h5>Tu van 24/7</h5>
-                                    <p>Luon luon co nhan vien tu van va ho tro cham soc khach hang</p>
+                                    <h5>Tư vấn 24/7</h5>
+                                    <p>Luôn luôn có nhân viên tư vấn và hỗ trợ chăm sóc khách hàng</p>
                                 </div>
                             </div>
                         </div>
